270f0d2f01068a8583ab2fd80dbe1c5b6ee74a76
Huynh <1474202@fcps.edu>
2018-09-29 17:49:58 -0400

    my project

--- routes/controller.js
+++ routes/controller.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var Profile = require('../models').profile
-//var Profile = require('../mock/profile')
+//var Profile = require('../models').profile
+var Profile = require('../mock/profile')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendStatus(200)
+  res.send('hello world')
 });
 
+router.get('/profiles', function (req, res, next) {
+  res.send(Profile.findAll())
+});
+
+router.get('/profiles/:id', function (req, res) {
+  var profile = Profile.findById(req.params.id)
+  res.send(profile)
+})
+
+router.delete('/profiles/:id', function(req, res) {
+  var profiles = Profile.remove(req.params.id)
+  res.send(profiles)
+})
+
+router.post('/profiles', function (req, res) {
+  var profiles = Profile.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    bio: req.body.bio,
+    interests: req.body.interests,
+    skills: req.body.skills
+  })
+  res.send(profiles)
+})
  
 module.exports = router;
